8a731e01631fec119798d9fa5a451a26b73306cf
Dang Tieu Long <dangtieulong362@gmail.com>
2025-03-02 23:01:38 +0700

    video 22

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hello World + Long</h1>
+      <h1>Hello World ne</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
