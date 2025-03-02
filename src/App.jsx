06b2174eff18bfc28417ff25163aaf9c0cda2c57
Dang Tieu Long <dangtieulong362@gmail.com>
2025-03-02 23:36:26 +0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+
+// () => {}
+const App = () => {
   const [count, setCount] = useState(0)
+
+  const myFunction = () => {
+    console.log('Hello World')
+  }
+
+  myFunction()
 
   return (
     <>
