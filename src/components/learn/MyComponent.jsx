4dd7adf6f1de9de0e60e3fdc47e7ffdfd554e545
Dang Tieu Long <dangtieulong362@gmail.com>
2025-03-03 13:25:50 +0700

    jsx

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,9 +2,20 @@
 import './style.css';
 
 const MyComponent = () => {
+    // const dragonaxii = "long ne";
+    // const dragonaxii = 25;
+    // const dragonaxii = true;
+    // const dragonaxii = undefined;
+    // const dragonaxii = null;
+    const dragonaxii = [1, 2, 3]
+    // const dragonaxii = {
+    //     name: "long",
+    //     age: 25
+    // }
     return (
         <>
-            <div>long ne</div>
+            <div>{JSON.stringify(dragonaxii)} lai lan nua ne</div>
+            <div>{console.log("long ne")}</div>
             <div className="child" style={{borderRadius: "10px"}}>Child</div>
         </>
         
