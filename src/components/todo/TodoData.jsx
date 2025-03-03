ccf54435667ced0fe91c763b69bef2365e2d4cb2
Dang Tieu Long <dangtieulong362@gmail.com>
2025-03-03 14:29:19 +0700

    todo form

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,11 @@
+
+const TodoData = () => {
+    return (
+        <div className="todo-data">
+            <div>Learning React</div>
+            <div>Learning Spring boot</div>
+        </div>
+    )
+}
+
+export default TodoData
---
+++ src/components/todo/TodoForm.jsx
@@ -0,0 +1,11 @@
+
+const TodoForm = () => {
+    return (
+        <div className="todo-form">
+            <input type="text" placeholder="Add new todo" />
+            <button>Add</button>
+        </div>
+    )
+}
+
+export default TodoForm;
